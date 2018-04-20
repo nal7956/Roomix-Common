779531104de664094061762f9f3810abbd11b85a
Daniel Kalb <36692390+kalbdaniel@users.noreply.github.com>
2018-04-20 14:48:40 +0200

    Add files via upload

---
+++ Database/Testdaten_TeamE/Season.sql
@@ -0,0 +1,4 @@
+insert into Season (SeasonID, Description, AdditionalCharge, StartDate, EndDate) values (null, 'SPRING', 26, '2018/03/20', '2018/06/20');
+insert into Season (SeasonID, Description, AdditionalCharge, StartDate, EndDate) values (null, 'AUTUMN', 26, '2018/09/23', '2018/12/20');
+insert into Season (SeasonID, Description, AdditionalCharge, StartDate, EndDate) values (null, 'SUMMER', 34, '2018/06/21', '2018/09/22');
+insert into Season (SeasonID, Description, AdditionalCharge, StartDate, EndDate) values (null, 'WINTER', 33, '2018/12/21', '2019/03/19');
